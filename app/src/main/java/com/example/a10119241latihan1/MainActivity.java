44294e8e3d824362afafb67965016b244dc2642d
LENOVO <ahmadfahmifahlevi22@gmail.com>
2022-04-18 14:21:54 +0700

    initial commit 10119241

---
+++ app/src/main/java/com/example/a10119241latihan1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a10119241latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+<!--Tanggal Pengerjaan : 18-19 April 2022-->
+<!--NIM : 10119241-->
+<!--Nama : Ahmad Fahmi Fahlevi-->
+<!--Kelas : IF6-->
---
+++ app/src/test/java/com/example/a10119241latihan1/ExampleUnitTest.java
@@ -0,0 +1,21 @@
+package com.example.a10119241latihan1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
+<!--Tanggal Pengerjaan : 18-19 April 2022-->
+<!--NIM : 10119241-->
+<!--Nama : Ahmad Fahmi Fahlevi-->
+<!--Kelas : IF6-->
